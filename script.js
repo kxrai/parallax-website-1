f88b93f6fe4b334321a21fad009c135919880889
kxrai <alicia.2005.loi@gmail.com>
2024-03-07 14:11:45 -0500

    template completed
    project is finished

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let moon = document.getElementById("moon");
 let text = document.getElementById("text");
 let train = document.getElementById("train");
 
-let dessert_moon = document.getElementById("dessert-moon");
+let desert_moon = document.getElementById("desert-moon");
 let man = document.getElementById("man");
 
 window.addEventListener("scroll", ()=>{
@@ -11,7 +11,31 @@ window.addEventListener("scroll", ()=>{
     text.style.top = 80 + value * -0.2 + "%";
     train.style.left = value * 1.5 + "px";
 
-    dessert_moon.style.top = value * .3 + "px";
+    desert_moon.style.top = value * .3 + "px";
     man.style.left = value * .6 + "px"
 
-})
+});
+
+// progress bar
+
+let calcScrollValue = ()=>{
+    let scrollProgress = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#194eb9 ${scrollValue}%,#67ccff ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
